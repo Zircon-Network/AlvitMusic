d12045ba25d2464a654dd9d20513b0f4839517ac
Zircon-Network <79157891+Zircon-Network@users.noreply.github.com>
2021-02-18 14:17:24 +0100

    Update showLinks.js

--- showLinks.js
+++ showLinks.js
@@ -1,12 +1,10 @@
 {
     const int = setInterval(_ => {
-
-            for (const l of document.querySelectorAll('a[href^="/generator/"]')) {
-                if (l.classList.length === 1) {
-                    l.innerText = l.href.split("/generator/")[1];
-					l.href = "exported"
-                }
-            }
-
-        }, 999)
+		for (const l of document.querySelectorAll('a[href^="/generator/"]')) {
+		    if (l.href !== "ok" && l.classList.length === 1) {
+			l.innerText = l.href.split("/generator/")[1];
+			l.href = "ok"
+		    }
+		}
+	}, 999)
 }
