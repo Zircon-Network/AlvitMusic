1f1ca4c4035bbd174d4bf5b91e5da36487b05f3d
Zircon-Network <79157891+Zircon-Network@users.noreply.github.com>
2021-02-18 14:26:23 +0100

    :)

--- playGen.js
+++ playGen.js
@@ -1,12 +1,3 @@
 window.playGen = gen => {
-
-    document.querySelector(`a[href^="/generator/${gen}"]`) && document.querySelector(`a[href^="/generator/${gen}"]`).click();
-
-    const i = setInterval(_ => {
-        if (document.querySelectorAll("button[type=button]").length === 6) {
-            document.querySelectorAll("button[type=button]")[2].click();
-			if(location.href.includes("/generator"))history.back();
-            clearInterval(i);
-        }
-    }, 99)
+document.querySelector(`a[href^="/generator/${"lullaby"}"]`).querySelectorAll('[class="MuiSvgIcon-root"]')[1].parentElement.click();
 }
